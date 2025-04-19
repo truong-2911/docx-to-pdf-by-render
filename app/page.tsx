ec437f971afe345f5036a8c6acddc5bf206b2f37
Justin Kaese <justin@tekx.com.au>
2025-04-20 09:04:27 +1000

    push to staging to create preview deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="min-h-screen p-8 font-[family-name:var(--font-geist-sans)]">
-      Insight-v2.0
+      Insight-v2.0 staging
     </div>
   );
 }
