7573faf29050d936dd728ddf47a367ae0486a9a2
Justin Kaese <justin@tekx.com.au>
2025-04-20 11:12:48 +1000

    add ENV to page and testing route

--- app/api/testing/route.ts
+++ app/api/testing/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  console.log(`[${process.env.ENV?.toUpperCase()}] Testing API`);
   return NextResponse.json({
     message: "Connection successful!",
     timestamp: new Date().toISOString(),
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="min-h-screen p-8 font-[family-name:var(--font-geist-sans)]">
-      Insight-v2.0 Dev
+      <p>{"Insight-v2.0 env: " + process.env.ENV}</p>
     </div>
   );
 }
